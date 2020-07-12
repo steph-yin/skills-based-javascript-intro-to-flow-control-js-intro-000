0679db29aca67c62f76a311b9a9d98792f70da68
Stephanie Yin <sstteepphhhhh@gmail.com>
2020-07-12 08:47:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,15 +5,15 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if (age < 13 || age > 19) {
-    return "You are not a teenager"
-  } else {
+  if (age > 13 && age < 19) {
     return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
   }
 }
 
 function ageChecker(age) {
-  if (age >= 13 && age =< 19) {
+  if (age > 13 && age < 19) {
     return "You are a teenager!"
   } else if (age <= 12) {
     return "You are a kid"
